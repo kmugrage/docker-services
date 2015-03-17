9d216fc4227083a444ee0e7982c9df2aeb265f39
Ken Mugrage <kmugrage@thoughtworks.com>
2015-03-17 11:47:31 +0000

    updated to use the user go in the dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM ubuntu
+RUN useradd go
 
 RUN apt-get update && apt-get install -y php5 && apt-get install -y phpunit
+
+USER go
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it -v=$PWD/workshop-services/:/var/www/html -w=/var/www/html kmugrage/workshop-services phpunit
+docker run --user="go" --rm -it -v=$PWD/workshop-services/:/var/www/html -w=/var/www/html kmugrage/workshop-services phpunit

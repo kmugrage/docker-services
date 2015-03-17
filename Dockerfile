bac34cd757112bc245db71554a6cbf7949dd9103
Ken Mugrage <kmugrage@thoughtworks.com>
2015-03-17 16:05:24 +0000

    changed run place

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM ubuntu
-RUN useradd go
 
+USER root
+RUN useradd go
 RUN apt-get update && apt-get install -y php5 && apt-get install -y phpunit
+
+COPY workshop-services /var/www/html/
+
+RUN chown -R go:go /var/www/html
 
 USER go
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --user="go" --rm -it -v=$PWD/workshop-services/:/var/www/html -w=/var/www/html kmugrage/workshop-services phpunit
+docker run --user="go" --rm -it -v=$PWD/workshop-services/:/workshop-services -w=/workshop-services kmugrage/workshop-services phpunit
